678b45fe60b5493d0d4c3b58acb9939796b36827
dalinwanglhtz <dalinwanglhtz@gmail.com>
2020-09-27 00:39:11 +1000

    Resume Header Added

--- force-app/main/default/lwc/myResumeContainer/myResumeContainer.js
+++ force-app/main/default/lwc/myResumeContainer/myResumeContainer.js
@@ -6,5 +6,4 @@ export default class MyResumeContainer extends LightningElement {
     PROFILE_IMAGE = RESUME_DATA.PROFILE_IMAGE;
     SOCIAL_LINKS = RESUME_DATA.SOCIAL_LINKS;
     RESUME_DATA = RESUME_DATA;
-
 }
---
+++ force-app/main/default/lwc/myResumeHeader/myResumeHeader.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+export default class MyResumeHeader extends LightningElement {
+    @api socialDetails;
+    @api userDetails;
+    @api profileImage;
+
+    get phoneLink() {
+        return `tel:${this.userDetails.PHONE}`;
+    }
+
+    get emailLink(){
+        return `mailto:${this.userDetails.EMAIL}`;
+    }
+}
